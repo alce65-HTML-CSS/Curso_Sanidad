88d2616ea97806b3037a53985076761dc01cd7a1
alce65 <alce65@hotmail.es>
2019-09-23 13:20:16 +0200

    Web inicial

---
+++ 02_inicio/js/app.js
@@ -0,0 +1,9 @@
+console.log('Iniciando JS desde un fichero')
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    let temp = document.querySelector('[rel="import"]')
+    .querySelector('template')
+    console.log(temp)
+
+})
